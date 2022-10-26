2377c474a05230b65a2e15d86ea64f7e8afd2df5
Aditya <adi78705483@gmail.com>
2022-10-26 13:49:46 +0530

    this is my profile

---
+++ Profile.py
@@ -0,0 +1,14 @@
+def student(Adi):
+    pro = [["Hi👋  I am Adi"],
+           ["💂‍♂️: I am a Cs student"]]
+    Adi  = "If at once you don’t succeed, CODE"
+    Github = "https://github.com/Ad1tyaRaj"
+    Twitter = 'https://twitter.com/ad1tya_raj_'
+    Linkedin = "https://www.linkedin.com/in/aditya-raj-07950a234/"
+    Instagram = "https://instagram.com/adi__.__?igshid=YmMyMTA2M2Y="
+    print(Adi)
+    print(pro)
+    print(f"Connect: Github:{Github}, Twitter:{Twitter}, Linkedin:{Linkedin}",
+          f"Instagram: {Instagram}")
+
+student("Cs student")
